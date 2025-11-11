b5af53fef6481ecd18758c223006d2d63993d956
liyunming <2762484966@qq.com>
2025-11-11 16:55:17 +0800

    只留疾病编码/病原微生物

--- src/services/unknownCase/me.ts
+++
@@ -1,7 +0,0 @@
-import { dhis2Client, buildFieldsParam } from '../../api/dhis2Client';
-import type { MeResponse } from './types';
-
-export async function getMe(): Promise<MeResponse> {
-  const fields = buildFieldsParam(['id,username,organisationUnits[id,name,path]']);
-  return dhis2Client.get<MeResponse>('/api/me', { fields });
-}
